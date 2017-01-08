714d1fe25fa872b20b78fa80cd6e6f14c9de58a0
Daniel Bayerlein <daniel.bayerlein@googlemail.com>
2017-01-08 17:28:26 +0100

    Add correction plugin

---
+++ correction.plugin.zsh
@@ -0,0 +1,17 @@
+#
+# Provides correction and defines aliases.
+#
+
+# Correct commands.
+setopt CORRECT
+
+# Disable correction.
+alias cd='nocorrect cd'
+alias cp='nocorrect cp'
+alias gcc='nocorrect gcc'
+alias grep='nocorrect grep'
+alias ln='nocorrect ln'
+alias man='nocorrect man'
+alias mkdir='nocorrect mkdir'
+alias mv='nocorrect mv'
+alias rm='nocorrect rm'
